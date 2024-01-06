149ef021e7967e39e2cf36178a084e25a4063faa
bhuwan <bkspoudel@gmail.com>
2024-01-06 09:05:16 +0100

    install json-server

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { AbcComponent } from './abc/abc.component';
+import { AllPostsComponent } from './post/all-posts/all-posts.component';
+import { CreateComponent } from './post/create/create.component';
+import { ViewComponent } from './post/view/view.component';
 
 export const routes: Routes = [
-
+  {
+    path: 'posts', component: AllPostsComponent,
+  },
+  {
+    path: 'create', component: CreateComponent,
+  },
+  {
+    path: 'view', component: ViewComponent,
+  }
 ];
