81c43765eda5d0779c856de6d6d0082a3c85490a
bhuwan <bkspoudel@gmail.com>
2024-01-09 21:06:19 +0100

    fix api call to apiService and login-signup services

--- src/app/shared/services/login-signup.service.ts
+++ src/app/shared/services/login-signup.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginSignupService {
+  public login_url = '';
+  public reg_url = '';
+  constructor(private  http: HttpClient, private apiService: ApiService) { }
 
-  constructor() { }
+  authLogin(user_name: any, password: any): Observable<any> {
+    return this.apiService.get(this.login_url + '/user?email=' + user_name + '&password=' + password);
+  }
+
+  userRegister(user_dto: any): Observable<any> {
+    return this.apiService.post(this.reg_url + '/user', user_dto);
+  }
+
+  adminLogin(user_name: any, password: any): Observable<any> {
+    return this.apiService.get(this.login_url + '/user?email='+user_name + '&password=' + password);
+  }
 }
