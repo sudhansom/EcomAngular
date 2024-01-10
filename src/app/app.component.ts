2fb2d915832274b16356ce664720461e540d4713
bhuwan <bkspoudel@gmail.com>
2024-01-10 14:03:51 +0100

    fix header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
