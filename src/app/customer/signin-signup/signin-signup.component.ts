2f238ab8af03feac624e566e54b1a3ac27116ede
bhuwan <bkspoudel@gmail.com>
2024-01-16 04:46:40 +0100

    signup form created

--- src/app/customer/signin-signup/signin-signup.component.ts
+++ src/app/customer/signin-signup/signin-signup.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signin-signup.component.html',
   styleUrl: './signin-signup.component.scss'
 })
 export class SigninSignupComponent {
+  signupForm!: FormGroup;
 
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      mobNumber: new FormControl('', Validators.required),
+      age: new FormControl(0, Validators.required),
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm.value);
+  }
 }
