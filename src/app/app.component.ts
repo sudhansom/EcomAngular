9a168b911cddd95266381f16ed6f3454284c7eb4
bhuwan <bkspoudel@gmail.com>
2024-01-17 13:04:56 +0100

    add login, Register and logout links in navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './shared/layouts/header/header.component';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
+import { ApiService } from './core/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,12 @@ import { FooterComponent } from './shared/layouts/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+  constructor(private apiService: ApiService){
+
+  }
+  ngOnInit(){
+    this.apiService.get('http://localhost:3000/users').subscribe(console.log);
+  }
 }
--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  logout(){
+    console.log('you are now logged out.');
+  }
 }
